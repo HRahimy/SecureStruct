8d309e29f64176c87c5148f6a2d2cdf0b5193f36
HRahimy <rahimy.hamza@gmail.com>
2024-05-04 00:14:17 +0300

    Complete `AuthorizeAsync` implementation

--- src/Infrastructure/Identity/KeycloakAuthorizerService.cs
+++ src/Infrastructure/Identity/KeycloakAuthorizerService.cs
@@ -1,6 +1,5 @@
-﻿using System.Net.Http;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using SecureStruct.Application.Common.Interfaces;
 
 namespace SecureStruct.Infrastructure.Identity;
@@ -12,47 +11,45 @@ public class KeycloakAuthorizerService : IAuthorizerService
         _params = options.Value;
     }
 
-    public async Task<bool> AuthorizeAsync(string token, List<string>? permissions = null)
+    public async Task<bool> AuthorizeAsync(string authToken, List<string>? permissions = null)
     {
         permissions ??= [];
 
         using (HttpClient client = new HttpClient())
         {
-            token = token.Split(" ")[1];
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            client.DefaultRequestHeaders.Add("Authorization", authToken);
 
-            var contentDict = new Dictionary<string, string>
+            var contentDict = new List<KeyValuePair<string, string>>
             {
-                { "grant_type", "urn:ietf:params:oauth:grant-type:uma-ticket" },
-                { "audience", _params.ClientId },
-                { "response_mode", "decision" }
+                new( "grant_type", "urn:ietf:params:oauth:grant-type:uma-ticket" ),
+                new( "audience", _params.ClientId ),
+                new( "response_mode", "decision" ),
             };
 
+            foreach (var permission in permissions)
+            {
+                contentDict.Add(new KeyValuePair<string, string>("permission", permission));
+            }
+
             string url = _params.KeycloakUrl + $"/realms/{_params.Realm}/protocol/openid-connect/token";
+
             var request = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(contentDict) };
-
-            var requestString = request.ToString();
-            var requestContent = await request.Content.ReadAsStringAsync();
             var response = await client.SendAsync(request);
-
-
-            //var response = await client.PostAsync(
-            //    _params.KeycloakUrl + $"/realms/{_params.Realm}/protocol/openid-connect/token",
-            //    new FormUrlEncodedContent(contentDict)
-            //);
 
             if (response.IsSuccessStatusCode)
             {
                 // Read and display the response content
                 string responseContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("Response: " + responseContent);
+                var parsedContent = JsonConvert.DeserializeObject<Dictionary<string, object>>(responseContent) ?? throw new Exception("Failed to parse Authorization response.");
+                if (parsedContent["result"] == null) throw new Exception("Could not find `result` property in Authorization response");
+                if (parsedContent["result"] is not bool) throw new Exception("Could not find `result` property in Authorization response");
+
+                return (bool)parsedContent["result"];
             }
             else
             {
-                Console.WriteLine("Error: " + response.StatusCode);
+                return false;
             }
         }
-
-        return false;
     }
 }
